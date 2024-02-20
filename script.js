aef8e852a853b806860520e8b5a9490b6bd8e43b
bodhr01 <bodhr0101@gmail.com>
2024-02-20 18:39:34 +0530

    FP

---
+++ script.js
@@ -0,0 +1,19 @@
+const countvalue=document.querySelector('#counter');
+
+const increment =() => {
+    //get the value from ui
+    let value=parseInt(countvalue.innerText);
+   //update the value
+   value=value+1;
+   //set the value onto UI
+   countvalue.innerText=value;
+};
+
+const decrement = () =>{
+    //get the value
+    let value=parseInt(countvalue.innerText);
+    //update the value
+    value=value-1;
+    //set the value onto UI
+    countvalue.innerText=value;
+};
